56e8574fd56350ee626c10dd371ecaad8d1edf1f
renshijie <1192306691@qq.com>
2025-01-13 14:42:25 +0800

    cs

--- app/src/main/java/com/easybuilder/base/pages/ApiTestActivity.kt
+++ app/src/main/java/com/easybuilder/base/pages/ApiTestActivity.kt
@@ -24,7 +24,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.IOException
 
-class ApiTestActivity : BaseVMActivity<ActivityApiTestBinding,MainViewModel>(
+class ApiTestActivity : BaseVMActivity<ActivityApiTestBinding, MainViewModel>(
     viewModelClass = MainViewModel::class.java
 ) {
     private var testKotlinAdapter: TestKotlinAdapter? = null
@@ -40,15 +40,66 @@ class ApiTestActivity : BaseVMActivity<ActivityApiTestBinding,MainViewModel>(
             }
         });
 
-        Glide.with(this).load("https://img0.baidu.com/it/u=1008951549,1654888911&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800").into(mBinding.ivHead)
+        Glide.with(this)
+            .load("https://img0.baidu.com/it/u=1008951549,1654888911&fm=253&fmt=auto&app=120&f=JPEG?w=800&h=800")
+            .into(mBinding.ivHead)
+
+
+//        lifecycleScope.launch {
+//            val result1 = withContext(Dispatchers.IO) {
+//                performNetworkRequest("1")
+//            }
+//            println("Result 1: $result1")
+//
+//            val result2 = withContext(Dispatchers.IO) {
+//                performNetworkRequest("2")
+//            }
+//            println("Result 2: $result2")
+//
+//            val result3 = withContext(Dispatchers.IO) {
+//                performNetworkRequest("3")
+//            }
+//            println("Result 3: $result3")
+//
+//            val result4 = withContext(Dispatchers.IO) {
+//                performNetworkRequest("4")
+//            }
+//            println("Result 4: $result4")
+//        }
+
+
+        lifecycleScope.launch {
+            val result1 = async(Dispatchers.IO){
+                performNetworkRequest("1")
+                println("Result1-----------")
+            }
+            val result2 = async(Dispatchers.IO){
+                performNetworkRequest("2")
+                println("Result2-----------")
+            }
+            val result3 = async(Dispatchers.IO){
+                performNetworkRequest("3")
+                println("Result3-----------")
+            }
+            val result4 = async(Dispatchers.IO){
+                performNetworkRequest("4")
+                println("Result4-----------")
+            }
+//            println("Result 1: ${result1.await()}")
+//            println("Result 2: ${result2.await()}")
+//            println("Result 3: ${result3.await()}")
+//            println("Result 4: ${result4.await()}")
+        }
+
+        println("Result next!!!")
     }
 
     override fun loadData() {
-        mBinding.rv.layoutManager= LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        mBinding.rv.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         testKotlinAdapter = TestKotlinAdapter(this)
         mBinding.rv.adapter = testKotlinAdapter
 
-        testKotlinAdapter?.setDataList(mutableListOf("1","2","3"))
+        testKotlinAdapter?.setDataList(mutableListOf("1", "2", "3"))
 
     }
 
@@ -63,10 +114,10 @@ class ApiTestActivity : BaseVMActivity<ActivityApiTestBinding,MainViewModel>(
             delay(2000)
         }
         val test = mViewModel.sampleRepo.test()
-        return "$request   ${ test!!.body()!!.message }"
+        return "$request   ${test!!.body()!!.message}"
     }
 
-//    override fun onBackPressed() {
+    //    override fun onBackPressed() {
 //        super.onBackPressed()
 //        "测试".log()
 //    }
